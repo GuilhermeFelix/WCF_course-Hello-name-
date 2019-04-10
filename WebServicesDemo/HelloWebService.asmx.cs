d0ada1da591eff0506c1cdaeb342f41430ce090a
Guilherme Felix da Silva <42627524+GuilhermeFelix@users.noreply.github.com>
2019-04-09 21:55:07 -0300

    Class 1
    Introduction to webservices, teaching to create a simple service to join the word "Hello" with the name entered by the user. In this lesson I explain how the Get of the service works.

---
+++ WebServicesDemo/HelloWebService.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServicesDemo
+{
+    /// <summary>
+    /// Descrição resumida de HelloWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que esse serviço da web seja chamado a partir do script, usando ASP.NET AJAX, remova os comentários da linha a seguir. 
+    // [System.Web.Script.Services.ScriptService]
+    public class HelloWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string GetMessage(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
